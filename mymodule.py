990fe3aa9b2e8a0a30a2755949db69467672da20
zewsquaz <alexey.tsedik@yandex.ru>
2020-10-08 16:02:14 +0300

    module

---
+++ mymodule.py
@@ -0,0 +1,12 @@
+
+def fuckoff( n ):
+    
+   print ("Fuck off %s  get the fuck out of here" % n )
+   res=n + " Kozel"
+   return res
+
+
+
+def summa(x, y):
+   return x + y
+   
---
+++ usermoule.py
@@ -0,0 +1,11 @@
+
+import mymodule
+
+print ("\n")
+kozvar= "\n" + mymodule.fuckoff("Galaction") + "\n"
+sumvar= " 12 + 7 = " + str( mymodule.summa(12,7) )
+
+print (sumvar)
+print (kozvar)
+
+
