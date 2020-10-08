3966f2cfdc625d6f3a3d3133b8f00fa4ef8c5a43
zewsquaz <alexey.tsedik@yandex.ru>
2020-10-08 11:47:46 +0300

    test

--- test.py
+++ test.py
@@ -1,28 +1,20 @@
 a = 10
-
-b = 5
+b = 6
 
 if a > b:
-
-print("A больше B")
-
-print(a - b)
+   print("A > B")
+   print(a - b)
 
 else:
-
-print("B больше или равно A")
-
-print(b - a)
+   print("B <= A")
+   print(b - a)
 
 print("The End")
 
 def open_file(filename):
+   print("Reading file", filename)
+   with open(filename) as f:
 
-print("Reading file", filename)
-
-with open(filename) as f:
-
-return f.read()
-
-print("Done")
+      return f.read()
+      print("Done")
 
