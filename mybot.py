c457d925228f35be7070a4f1d069caaf523979fc
zewsquaz <alexey.tsedik@yandex.ru>
2023-03-26 10:03:00 +0300

    ne bot

---
+++ mybot.py
@@ -0,0 +1,16 @@
+
+import telebot
+
+print("My first telegram bot")
+
+bot = telebot.TeleBot("5609240263:AAGUCKB-TEa3YOdfLyMH0T2Qy6mZPAOD9QU")
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+    bot.reply_to(message, "Да, "+ message.text)
+    print(message.text, type( message.text ))
+
+
+bot.polling( none_stop = True )
+
+
